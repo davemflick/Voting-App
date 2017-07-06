61252151f8b8c8a23e71cfe0b1297afcce5be4da
davemflick <davemflick@yahoo.com>
2017-07-06 16:25:03 -0400

    starting to build react components

--- src/app.js
+++ src/app.js
@@ -1,12 +1,19 @@
 import React, { Component } from 'react';
 import ReactDOM from 'react-dom';
+import Polls from './components/Polls';
 
 class App extends Component{
 
 
 	render(){
 		return (
-			<div> Hello from React </div>
+			<div> 
+				<p>Hello from React </p>
+				<ul>
+					<Polls />
+				</ul>
+			</div>
+
 		)
 	}	
 }
